bea30bc3fe8adf8728b634508f6eac578db64e71
huyfr <61008601+huyfr@users.noreply.github.com>
2020-02-21 11:41:35 +0700

    Sửa bải tập sử dụng Switch case lần 2

--- calculateDaysInAMonth.js
+++ calculateDaysInAMonth.js
@@ -7,40 +7,22 @@ function execute()
     switch (inputNumber)
     {
         case 1:
-            document.getElementById("result").innerHTML = "Thang 1 co 31 ngay";
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            document.getElementById("result").innerHTML = "Thang " + inputNumber + " co 31 ngay";
             break;
         case 2:
-            document.getElementById("result").innerHTML = "Thang 2 co 28 hoac 29 ngay";
-            break;
-        case 3:
-            document.getElementById("result").innerHTML = "Thang 3 co 31 ngay";
+            document.getElementById("result").innerHTML = "Thang " + inputNumber + " co 28 hoac 29 ngay";
             break;
         case 4:
-            document.getElementById("result").innerHTML = "Thang 4 co 30 ngay";
-            break;
-        case 5:
-            document.getElementById("result").innerHTML = "Thang 5 co 31 ngay";
-            break;
         case 6:
-            document.getElementById("result").innerHTML = "Thang 6 co 30 ngay";
-            break;
-        case 7:
-            document.getElementById("result").innerHTML = "Thang 7 co 31 ngay";
-            break;
-        case 8:
-            document.getElementById("result").innerHTML = "Thang 8 co 31 ngay";
-            break;
         case 9:
-            document.getElementById("result").innerHTML = "Thang 9 co 30 ngay";
-            break;
-        case 10:
-            document.getElementById("result").innerHTML = "Thang 10 co 31 ngay";
-            break;
         case 11:
-            document.getElementById("result").innerHTML = "Thang 11 co 30 ngay";
-            break;
-        case 12:
-            document.getElementById("result").innerHTML = "Thang 12 co 31 ngay";
+            document.getElementById("result").innerHTML = "Thang " + inputNumber + " co 30 ngay";
             break;
         default:
             alert("Vui long nhap lai!");
